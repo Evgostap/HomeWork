6a51dee55f05028c1fea606345ad0595617baddf
Evgeny Ostapets <evgostap@mail.ru>
2019-11-11 14:20:10 +0300

    Домашнее задание к четвертому уроку

---
+++ lesson04/HomeWork/src/geekbrains/homework_5/persArray.java
@@ -0,0 +1,17 @@
+package geekbrains.homework_5;
+
+public class persArray {
+     String name, position, mail;
+     int phone, salary, age;
+
+
+    public persArray(String name, String position, String mail, int phone, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.mail = mail;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+}
